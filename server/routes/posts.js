e1ad849422d139189269229de39eaa4182376a71
Chickenboy71 <67282209+Chickenboy71@users.noreply.github.com>
2023-09-30 14:00:00 +0800

    ;

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,13 +2,13 @@ import express from "express";
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
-const router = express.Router()
+const router = express.Router();
 
 /* READ */
-router.get("/", verifyToken, getFeedPosts)
-router.get("/:id", verifyToken, getUserPosts)
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:id", verifyToken, getUserPosts);
 
 /* UPDATE */
-router.patch("/:id/like", verifyToken, likePost)
+router.patch("/:id/like", verifyToken, likePost);
 
-export default router
+export default router;

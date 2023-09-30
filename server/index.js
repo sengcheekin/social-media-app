543feceef564baf24cb439421f18587aa7fadc0a
Chickenboy71 <67282209+Chickenboy71@users.noreply.github.com>
2023-09-30 19:05:50 +0800

    insert dummy data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url); // to get filename, specific to type="module"
@@ -60,6 +63,10 @@ mongoose
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server Port: ${PORT}`);
+
+      /* Populate database with dummy data. Make sure to only do this once. */
+      // User.insertMany(users);
+      // Post.insertMany(posts);
     });
   })
   .catch((error) => {

00a6c4ee0c96aa3ddb60a0692e1df86f1d5e38b1
Chickenboy71 <67282209+Chickenboy71@users.noreply.github.com>
2023-09-30 13:58:01 +0800

    Posts route, create, get and like

---
+++ server/controllers/posts.js
@@ -0,0 +1,73 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/* CREATE */
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    // Once the post is created and saved, retrieve all the posts and send them back to the frontend
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ message: error.message }); // 409 means something is wrong with the request
+  }
+};
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true } // new: true means return the updated post
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url); // to get filename, specific to type="module"
@@ -40,10 +43,12 @@ const upload = multer({ storage: storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register); // this route is placed here because we want to use the upload middleware
+app.post("/posts", verifyToken, upload.single("picture"), createPost); // this route is placed here because we want to use the upload middleware
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE */
 const PORT = process.env.PORT || 6001;
